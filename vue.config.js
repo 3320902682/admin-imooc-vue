056b2b94364142b761275805adb06ead15fbb103
TuYuhao <3320902682@qq.com>
2020-04-17 17:15:13 +0800

    4-17

--- vue.config.js
+++ vue.config.js
@@ -35,6 +35,16 @@ module.exports = {
     overlay: {
       warnings: false,
       errors: true
+    },
+    proxy: { // 为天气接口配置代理，解决跨域
+      '/api': {
+        'target':process.env.VUE_APP_BASE_API,
+        'secure': false, // false为http访问，true为https访问
+        'changeOrigin': true, // 跨域访问设置，true代表跨域
+        'pathRewrite': { // 路径改写规则
+          '^/api': '' // 以/api为开头的改写为''
+        }
+      }
     }
   },
   configureWebpack: {
